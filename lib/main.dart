0b29ad7ee8ae85305e87ca19c0edb67dd5a9bdcf
Kaung Pyae Min Thein <kaungpyae.kg@gmail.com>
2021-11-16 11:47:00 +0630

    finish dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,54 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  //const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  int rollDice() {
+    return Random().nextInt(6) + 1;
+  }
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = rollDice();
+      rightDiceNumber = rollDice();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png',
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset(
+                'images/dice$rightDiceNumber.png',
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
